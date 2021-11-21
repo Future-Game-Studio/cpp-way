c21ea27d7102c5c150006f9925fe52677ace3065
MaxRev <cmthea32@gmail.com>
2021-11-21 23:02:39 +0200

    adjust for logger changes

--- src/pointers/pointers.cpp
+++ src/pointers/pointers.cpp
@@ -26,47 +26,48 @@ void swap(int &x, int &y)
 
 int main()
 {
+    const auto logger = &std::cout;
     int origin = 10;
 
     int *a;
     a = &origin;
 
-    std::cout << "Pointer address: " << a << std::endl;
-    std::cout << "Pointer value: " << *a << std::endl;
+    *logger << "Pointer address: " << a << std::endl;
+    *logger << "Pointer value: " << *a << std::endl;
 
     int &b = *a;
 
-    std::cout << "Reference b: " << b << std::endl;
+    *logger << "Reference b: " << b << std::endl;
     int &c = b;
 
-    std::cout << "Reference c: " << c << std::endl;
+    *logger << "Reference c: " << c << std::endl;
 
     origin = 7;
-    std::cout << "Reference c (origin changed): " << c << std::endl;
+    *logger << "Reference c (origin changed): " << c << std::endl;
     // b = nullptr; //won't compile
 
     a = nullptr; // remember a and c ??
-    std::cout << "Pointer address a (null): " << a << std::endl;
+    *logger << "Pointer address a (null): " << a << std::endl;
 
     int **M;        // 2D matrix
     int ***pM = &M; // pointer to matrix
 
     a = &origin; // without this line there would be segmentation fault
 
-    std::cout << "Reference b (null ref): " << b << std::endl;
+    *logger << "Reference b (null ref): " << b << std::endl;
 
     // references can not be null. pointers can
     if (a != nullptr)
     {
         int f = 4;
         b = f;
-        std::cout << "Reference b (before method call): " << b << std::endl;
-        std::cout << "Pointer a (before method call): " << a << std::endl;
+        *logger << "Reference b (before method call): " << b << std::endl;
+        *logger << "Pointer a (before method call): " << a << std::endl;
         MethodCallWithRefParameter(b, a);
-        std::cout << "Reference b (after method call): " << b << std::endl;
-        std::cout << "Pointer a (after method call): " << *a << std::endl;
+        *logger << "Reference b (after method call): " << b << std::endl;
+        *logger << "Pointer a (after method call): " << *a << std::endl;
         MethodCallWithCopyParameter(*a);
-        std::cout << "Reference b (after copy call): " << b << std::endl;
+        *logger << "Reference b (after copy call): " << b << std::endl;
     }
 
     const int *pconst;
@@ -75,9 +76,9 @@ int main()
     /// int val = pconst++; // error const pointer is not arithmetic.
     const int *const pconst2 = &b;
     // pconst2 = &origin; // pointer can not be reassigned
-    std::cout << "Reference const p : " << b << std::endl;
+    *logger << "Reference const p : " << b << std::endl;
 
-    std::cout << std::endl;
+    *logger << std::endl;
 
     SmartUniquePtr();
     SmartSharedPtr();
